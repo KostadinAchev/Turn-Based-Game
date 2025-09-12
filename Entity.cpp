06e55cb54b6163a22000181fc171979c41f73556
KostadinAchev <koko_a4ev@abv.bg>
2025-09-12 23:15:53 +0300

    added an int to keep track of deadBodyCount from Entity.hpp
    changed all armor to armorDurability for clearance
    
    changed Entity::death() so if death is called for an object to not increment dead body more than once
    
    changed Entity::Attack() so it checks if the target or the attacker is dead or alive before attacking

--- Entity.cpp
+++ Entity.cpp
@@ -1,93 +1,123 @@
+#ifndef ENTITY_CPP
+#define ENTITY_CPP
 #include "Entity.hpp"
- 
-void Entity::Damage_Reduction(Entity *target){
-        float reduction = (100 - target->get_armorType()) / 100.0f;
-        float healthAfterDmg = target->get_health() - (get_damage() * reduction);
-        target->set_health(healthAfterDmg);                   
-        std::cout << get_type() << " Delt: " << get_damage() << " damage to: " << target->get_type() << '\n' << target->get_type() << "'s Health remaining is: "<< target->get_health() << '\n' << get_type() <<" mana remaining: "<< get_mana() <<'\n';
-        target->set_armor(target->get_armor() - 1);
-        std::cout << '\n' << "========================================================================================"<< '\n';
+
+int Entity::deadBodyCount = 0;
+
+void Entity::Damage_Reduction(Entity *target)
+{
+    float reduction = (100 - target->get_armorType()) / 100.0f;
+    float healthAfterDmg = target->get_health() - (get_damage() * reduction);
+    target->set_health(healthAfterDmg);
+    std::cout << get_type() << " Delt: " << get_damage() << " damage to: " << target->get_type() << '\n'
+              << target->get_type() << "'s Health remaining is: " << target->get_health() << '\n'
+              << get_type() << " mana remaining: " << get_mana() << '\n';
+    target->set_armorDurability(target->get_armorDurability() - 1);
+    std::cout << '\n'
+              << "========================================================================================" << '\n';
 }
 
 void Entity::Damage_NoReduction(Entity *target)
 {
     float healthAfterDmg = target->get_health() - get_damage();
     target->set_health(healthAfterDmg);
-    std::cout << get_type() << " Delt: " << get_damage() << " damage to: " << target->get_type() << '\n' << target->get_type() << "'s Health remaining is: "<< target->get_health() << '\n' << get_type() <<" mana remaining: "<< get_mana() <<'\n';
-    std::cout << '\n' << "========================================================================================"<< '\n';
+    std::cout << get_type() << " Delt: " << get_damage() << " damage to: " << target->get_type() << '\n'
+              << target->get_type() << "'s Health remaining is: " << target->get_health() << '\n'
+              << get_type() << " mana remaining: " << get_mana() << '\n';
+    std::cout << '\n'
+              << "========================================================================================" << '\n';
 }
 
-void is_alive_check(Entity *target)
+void Entity::is_alive_check(Entity *target)
 {
     if (target->get_health() <= 0)
     {
-    target->set_is_alive(false);
+        target->set_is_alive(false);
     }
 }
 // Done but idk if logic is good may need further improvements
-void Entity::manaRegen(){
-    
+void Entity::manaRegen()
+{
+
     int manaPerTurn = 75;
     set_mana(get_mana() + manaPerTurn);
-    std::cout << "Mana after regeneration: "<<get_mana() << '\n';
+    std::cout << "Mana after regeneration: " << get_mana() << '\n';
 }
 
 void Entity::death()
 {
-        set_health(0);
-        std::cout << get_type() << " died" << std::endl;
+    if (!get_is_alive())
+        return;
+
+    set_health(0);
+    set_is_alive(false);
+    std::cout << get_type() << " died" << std::endl;
+    Entity::incrementDeadBody();
 }
 
 void Entity::Attack(Entity *target)
+{
+    if (target->get_is_alive() != true || get_is_alive() != true)
     {
-        float reduction =0;
-        float healthAfterDmg=0;
-        if (target->get_is_alive() == true && get_is_alive() == true)
-        {
-            is_alive_check(target);
-            
-            if (get_damage() <= 0)
-            {
-                std::cout << "No Damage Delt!" << std::endl;
-            }
+        std::cout << " Target is: " << (target->get_is_alive() ? "Alive" : "Dead") << " Attacker is : " << (get_is_alive() ? "Alive" : "Dead");
+        return;
+    }
+    if (target->get_is_alive() == true && get_is_alive() == true)
+    {
+        is_alive_check(target);
 
+        if (get_damage() <= 0)
+        {
+            std::cout << "No Damage Delt!" << std::endl;
+        }
+        else
+        {
+            // has armor
+            if (target->get_armorDurability() > 0)
+            {
+                // damage reduction func
+                Damage_Reduction(target);
+                is_alive_check(target);
+            }
             else
             {
-                //has armor 
-                if(target->get_armor() > 0)
-                {
-                    //damage reduction func
-                    Damage_Reduction(target);
-                    is_alive_check(target); 
-                }
-                else
-                {
-                    //damage with no reduction
-                    Damage_NoReduction(target);
-                    //is alive check
-                    is_alive_check(target);
-                
-                }
-
+                // damage with no reduction
+                Damage_NoReduction(target);
+                // is alive check
+                is_alive_check(target);
             }
         }
-        else if (target->get_is_alive() == false)
+    }
+    else if (target->get_health() <= 0)
+    {
+        if (target->get_is_alive() == false)
         {
+            std::cout << "Target is already dead!" << std::endl;
+            return;
+        }
+        else
+        {
+            target->set_is_alive(false);
             target->death();
         }
     }
-
-void Entity::printInfo(){
-    
-    std::cout << " Type: " << get_type() << " | Health " << get_health() << " | Armor Type " << getArmorType(get_armorType())  << " | Armor " << get_armor() << " | Damage " << get_damage() << " | Cost " << get_moneyCost() << " | Mana " << get_mana() << " | Mana State " << (get_manaBool()? " True " : " False ") << " | Alive State " << get_is_alive() << std::endl;
-    std::cout << '\n' << "========================================================================================"<< '\n';
-    
 }
-    
+
+void Entity::printInfo()
+{
+
+    std::cout << " Type: " << get_type() << " | Health " << get_health() << " | Armor Type " << getArmorType(get_armorType()) << " | Armor " << get_armorDurability() << " | Damage " << get_damage() << " | Cost " << get_moneyCost() << " | Mana " << get_mana() << " | Mana State " << (get_isCaster() ? " True " : " False ") << " | Alive State " << get_is_alive() << std::endl;
+    std::cout << '\n'
+              << "========================================================================================" << '\n';
+}
 
 void Entity::Heal(Entity *target)
 {
-    float healing=0;
-    target->set_health(get_health()+ healing);
-    target->set_mana(get_mana()- get_minManaSpell());
+    target->set_mana(get_mana() - get_minManaSpell());
 }
+
+void Entity::healingDone(Entity *target, float healing)
+{
+    target->set_health(get_health() + healing);
+}
+#endif
