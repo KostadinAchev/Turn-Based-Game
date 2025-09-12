f752e5766a2528bf446f63ecc6b7cc7746c7c04b
KostadinAchev <koko_a4ev@abv.bg>
2025-09-12 23:26:53 +0300

    changed the c-tor entry with an enum with set values

--- Zombie.hpp
+++ Zombie.hpp
@@ -7,7 +7,7 @@
 class Zombie : public Entity{
 public:
 //    type || health || maxHP || armorType || armor || goodOrBad || damage || moneyCost || mana || maxMP || minManaSpell || manaBool || isAlive
-    Zombie() : Entity("Zombie", 250, 250, UNARMORED, 0, BAD, 15, 300, 0, 0, 0, false, true){};
+    Zombie() : Entity(ZOMBIE){};
 };
 
 #endif
