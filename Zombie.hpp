3595508d57f106833177896718118b4efdecba3c
KostadinAchev <koko_a4ev@abv.bg>
2025-09-29 14:42:14 +0300

    added comments for clearance

--- Zombie.hpp
+++ Zombie.hpp
@@ -3,11 +3,15 @@
 #include <iostream>
 #include "Entity.hpp"
 
+// different classes that inherit Entity
+// Entity constructor gets and Enum and sets the values for the different units
 
-class Zombie : public Entity{
+// type of Entity that can be bought and it has different subclasses
+class Zombie : public Entity
+{
 public:
-//    type || health || maxHP || armorType || armor || goodOrBad || damage || moneyCost || mana || maxMP || minManaSpell || manaBool || isAlive
-    Zombie() : Entity(ZOMBIE){};
+    //    type || health || maxHP || armorType || armor || goodOrBad || damage || moneyCost || mana || maxMP || minManaSpell || manaBool || isAlive
+    Zombie() : Entity(ZOMBIE) {};
 };
 
 #endif
